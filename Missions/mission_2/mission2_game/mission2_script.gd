1d381af06e9b35907ccfd3d1c09a1979ddc854bb
blake hardee <bhardee@unc.edu>
2024-04-18 11:07:51 -0400

    made each card a button and added triggers for each one

--- Missions/mission_2/mission2_game/mission2_script.gd
+++ Missions/mission_2/mission2_game/mission2_script.gd
@@ -1,12 +1,47 @@
 extends Node
 
+@onready var cyber_bullying = get_node("Cyber Bullying")
 
-# Called when the node enters the scene tree for the first time.
+var selected = []
+var stylebox_flat := StyleBoxFlat.new()
+
 func _ready():
+	stylebox_flat.border_width_left = 1
 	pass
 
+func _on_cyber_bullying_pressed():
+	cyber_bullying.add_stylebox_override("normal", stylebox_flat)
+	selected.append('cyber_bullying')
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
-	
+
+
+func _on_defamation_pressed():
+	pass # Replace with function body.
+
+
+func _on_disclosure_pressed():
+	pass # Replace with function body.
+
+
+func _on_exclusion_pressed():
+	pass # Replace with function body.
+
+
+func _on_identity_theft_pressed():
+	pass # Replace with function body.
+
+
+func _on_mobbing_culture_pressed():
+	pass # Replace with function body.
+
+
+func _on_instigation_pressed():
+	pass # Replace with function body.
+
+
+func _on_online_deception_pressed():
+	pass # Replace with function body.
+
+
+func _on_online_harassment_pressed():
+	pass # Replace with function body.
