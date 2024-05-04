cf6a6a06c7bc5ff2f1c17cd9cee9c8f821b7374c
nylaanderson <69765415+nylaanderson@users.noreply.github.com>
2024-05-04 15:35:49 -0400

    Added game logic for mission2_activity

--- Missions/mission_2/mission2_activity/matching_scenes/Buttons.gd
+++ Missions/mission_2/mission2_activity/matching_scenes/Buttons.gd
@@ -1,26 +1,124 @@
-extends Button
+extends Node
+
+
+
+@onready var enter = get_tree().get_root().get_node("SocialMedia/Enter")
+
+@onready var cyberbullying = get_tree().get_root().get_node("SocialMedia/PiecesContainer/HBoxContainer/Cyberbullying")
+
+@onready var next = get_tree().get_root().get_node("SocialMedia/Next")
+
+@onready var app = get_tree().get_root().get_node("SocialMedia/Application")
+
+@onready var message = get_tree().get_root().get_node("SocialMedia/Message")
+
+
 
 var unselected = true
-#var SelectedNames = ["CyberbullyingSelected.png", "DefamationSelected.png", "ExclusionSelected.png", ]
-# Called when the node enters the scene tree for the first time.
+
+var dict = read_file("res://Missions/mission_2/mission2_activity/applications.json")
+var item: Dictionary
+var item_index: int = 0
+
+var selected = [];
+
 func _ready():
 	
-	pass # Replace with function body.
+	next.set_disabled(true)
+	
+	var hbox_containers = [
+		get_tree().get_root().get_node("SocialMedia/PiecesContainer/HBoxContainer"),
+		get_tree().get_root().get_node("SocialMedia/PiecesContainer/HBoxContainer2"),
+		get_tree().get_root().get_node("SocialMedia/PiecesContainer/HBoxContainer3"),
+		get_tree().get_root().get_node("SocialMedia/PiecesContainer/HBoxContainer4")
+	]
+	for hbox in hbox_containers:
+		if hbox:
+			for button in hbox.get_children():
+				if button is Button:
+					button.pressed.connect(_on_pressed.bind(button)) # Replace with function body.
+	item = dict[item_index]
+	app.text = item.application
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
+func read_file(file):
+	var json_as_text = FileAccess.get_file_as_string(file)
+	var json_as_dict = JSON.parse_string(json_as_text)
+	return json_as_dict
 
-
-func _on_pressed():
-	if (unselected): 
-		self.set_button_icon(load("res://Images/Task 2 Selected Pieces/" + self.get_name() + "Selected.png"))
-		print("selected")
-		print(self.get_name())
-		unselected = false;
+func _on_pressed(button):
+	message.set_text("")
+	#if (unselected): 
+		#button.set_button_icon(load("res://Images/Task 2 Selected Pieces/" + self.get_name() + "Selected.png"))
+		#button.set_pressed(true)
+		#selected.append(self.get_name());
+		#print(selected)
+		#print(self.get_name())
+		#unselected = false;
+	if (button): 
+		if !selected.has(button):
+			selected.push_back(button)
+			button.set_button_icon(load("res://Images/Task 2 Selected Pieces/" + button.get_name() + "Selected.png"))
+			button.set_pressed(true)
+			print(selected)
+			print(button.get_name())
+			unselected = false;
+		else:
+			selected.erase(button)
+			button.set_button_icon(load("res://Images/Task 2 Unselected Pieces/" + button.get_name() + "Unselected.png"))
+			button.set_pressed(false)
+			print("unselected")
+			unselected = true;
+				
+func _is_correct():
+	var correct = 0
+	if(selected):
+		for button in selected:
+			if(item.answers.has(button.name)):
+				print(button.name)
+				correct += 1
+			else:
+				return false
+		if(correct == len(item.answers)):
+			return true
+		else:
+			return false
 	else:
-		self.set_button_icon(load("res://Images/Task 2 Unselected Pieces/" + self.get_name() + "Unselected.png"))
-		print("unselected")
-		unselected = true
+		return false
+		
+func _on_enter_pressed():
+	if(_is_correct()):
+		message.set_text("Good Job")
+		print("correct")
+		next.set_disabled(false)
+		
+	else:
+		message.set_text("Try Again")
+		_refresh_scene()
+		print("try again")
+		
+
+func _refresh_scene():
+	if (len(selected) > 0):
+		var buttons_to_reset = selected.duplicate()
+		for button in buttons_to_reset:
+			selected.erase(button)
+			button.set_button_icon(load("res://Images/Task 2 Unselected Pieces/" + button.get_name() + "Unselected.png"))
+
+				
+func _on_next_pressed():
+	item_index+=1
+	item = dict[item_index]
+	app.text = item.application
+	_refresh_scene()
+	#else:
+		#button.set_button_icon(load("res://Images/Task 2 Unselected Pieces/" + self.get_name() + "Unselected.png"))
+		#button.set_pressed(false)
+		#index = selected.indexof(self.get_name());
+		#selected.splice(index, 1);
+		#print("unselected")
+		#unselected = true
--- Missions/mission_2/mission2_activity/matching_scenes/social_media.gd
+++ Missions/mission_2/mission2_activity/matching_scenes/social_media.gd
@@ -1,9 +1,17 @@
 extends Node  
 
 
+
+
+var selected = []
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var button = get_tree().get_root().get_node("PiecesContainer/HBoxContainer/Cyberbullying")
+	
+	#var button = get_tree().get_root().get_node("SocialMedia/PiecesContainer/HBoxContainer/Cyberbullying")
+
+
 	#button.connect("pressed",_on_cyberbullying_pressed(self), 0)
 	#for piece in get_tree().get_nodes_in_group("PiecesContainer"):
 		#piece.connect("press", _on_piece_pressed(self), 0)
@@ -22,8 +30,7 @@ func _process(delta):
 			#piece.modulate = Color.AZURE;
 		##$name.set_button_icon(path)
 	
-	
-	
+
 	
 	
 	
@@ -45,3 +52,19 @@ func _process(delta):
 #func _on_defamation_pressed():
 	#pass # Replace with function body.
 
+
+
+
+
+#func _on_button_pressed():
+	#print("test")
+	#selected = buttons.selected 
+	#if (selected == item.answers):
+			#print("success") # Replace with function body.
+	#else:
+		#print("try again")
+
+
+
+	
+	 # Replace with function body.
